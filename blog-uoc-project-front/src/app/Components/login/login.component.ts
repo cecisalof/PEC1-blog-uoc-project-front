cdfaf935c3ff86209ff97f0d0d9f5c0924b4ee6b
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-10-17 12:24:32 +0200

    [Feature]: TODO 20

--- blog-uoc-project-front/src/app/Components/login/login.component.ts
+++ blog-uoc-project-front/src/app/Components/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder } from '@angular/forms';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth.service';
 import { HeaderMenusService } from 'src/app/Services/header-menus.service';
 import { LocalStorageService } from 'src/app/Services/local-storage.service';
 import { SharedService } from 'src/app/Services/shared.service';
+import { AuthDTO } from 'src/app/Models/auth.dto';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HeaderMenus } from 'src/app/Models/header-menus.dto';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +15,11 @@ import { SharedService } from 'src/app/Services/shared.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  /*
-  // TODO 19
   loginUser: AuthDTO;
   email: FormControl;
   password: FormControl;
   loginForm: FormGroup;
-  */
+
 
   constructor(
     private formBuilder: UntypedFormBuilder,
@@ -28,18 +29,25 @@ export class LoginComponent implements OnInit {
     private localStorageService: LocalStorageService,
     private router: Router
   ) {
-    // TODO 20
+    this.loginUser = {} as AuthDTO;
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]);
+    this.loginForm = this.formBuilder.group({
+      email: this.email,
+      password: this.password,
+    });
   }
 
   ngOnInit(): void {}
 
   async login(): Promise<void> {
-    /*
+    
     let responseOK: boolean = false;
     let errorResponse: any;
 
     this.loginUser.email = this.email.value;
     this.loginUser.password = this.password.value;
+    
     try {
       const authToken = await this.authService.login(this.loginUser);
       responseOK = true;
@@ -75,6 +83,6 @@ export class LoginComponent implements OnInit {
       this.headerMenusService.headerManagement.next(headerInfo);
       this.router.navigateByUrl('home');
     }
-    */
+
   }
 }
